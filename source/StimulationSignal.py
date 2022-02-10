8b9d9ba01b2f497b1fccb558807d4d1f93aa918a
Victorine Joncas <victorine.joncas@polymtl.ca>
2022-02-10 11:51:08 -0500

    Initialisation StimulationSignal
    Ajout des attributs

--- source/StimulationSignal.py
+++ source/StimulationSignal.py
@@ -2,5 +2,14 @@
 class StimulationSignal:
 
     # Constuctor
-    def __init__(self):
-        print("TODO")
+    def __init__(self, frequency, amplitude, pulse_width, training_time, muscle, electrode):
+        self.frequency = frequency
+        self.amplitude = amplitude
+        self.pulse_width = pulse_width
+        self.training_time = training_time
+        self.muscle = muscle
+        self.electrode = electrode
+        
+    def set_stimulation_signal(frequency, amplitude, pulse_width, training_time, muscle, electrode):
+        for i in range(frequency):
+            
