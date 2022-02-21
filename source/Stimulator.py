6a7947c558941bbba0b7615a45354ce71b9b6938
Victorine Joncas <victorine.joncas@polymtl.ca>
2022-02-21 18:09:19 -0500

    Ajout port avec Serial et Init_ACK
    Problème niveau data_length

--- source/Stimulator.py
+++ source/Stimulator.py
@@ -4,20 +4,28 @@
 from StimulationSignal import StimulationSignal
 import crccheck.checksum
 import numpy as np
+import serial #might need to do pip install serial
 
 # channel_stim:   list of active channels
 # freq:           main stimulation frequency in Hz (NOTE: this overrides ts1)
 # ts1:            main stimulation period in ms (1-1024.5 ms in 0.5 steps)
 # ts2:            inter-pulse time in ms (1.5-17 ms in 0.5 steps)
 
-
+port = serial.Serial(
+    baudrate=460800,
+    timeout=0.1, 
+    parity=serial.PARITY_EVEN,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS 
+)
+port.open()
 
 class Stimulator:
         # Constuctor
         #command = {'Init':0x01}
         version_number = 0x01
 
-        def __init__(self, channel_stim, freq, ts1, ts2, mode, pulse_width, amplitude):
+        def __init__(self, channel_stim, freq, ts1, ts2, mode, pulse_width, amplitude, force):
             self.channel_stim = channel_stim
             self.freq = freq
             self.ts1 = ts1
@@ -25,6 +33,7 @@ class Stimulator:
             self.mode = mode
             self.pulse_width = pulse_width
             self.amplitude = amplitude
+            self.force = force
             #self.baud_rate = 460800
             #self.start_byte = 0xF0
             #self.stop_byte = 0x0F
@@ -70,8 +79,20 @@ class Stimulator:
            return packet_data
             
         # Establishes connexion acknowlege
-        def init_ACK():
-            print("TODO")
+        def init_ACK(self):
+            packet = self.port.read
+            if packet == bytearray.fromhex(0xF0):
+                # Collect header bytes
+                for i in range(4):
+                    packet += self.port.read()
+                # Collect data bytes
+                datalength = Stimulator.data_length(packet_data) # vérifier je comprends pas trop comment ils ont fait
+                for i in range(datalength):
+                    packet += self.port.read()
+                # Collect stop byte
+                packet += self.port.read()
+                # Return packet as byte string
+            return packet
         
     
         # Sends message for unknown command
@@ -80,7 +101,7 @@ class Stimulator:
         
             
         # Error signal (inactivity ends connexion)    
-        def watchdog():
+        def watchdog(self):
             command = bytearray.fromhex(0x04)
             packet_number = np.zeros(255)
             for i in 255:
@@ -150,6 +171,14 @@ class Stimulator:
         # Error message sent by RehaStim2 
         def error():
             print("TODO")
+            
+        #Emergency stop
+        def emergency_stop(self, force):
+            if force == 0:
+                port.close()
+                
+            
+            
   
 
 #Essai
