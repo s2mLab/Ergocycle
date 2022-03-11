0d423e1555123323c437f7e91d94d12f2368934e
Frederique Leclerc <frederique-1.leclerc@polymtl.ca>
2022-03-11 09:02:20 -0500

    DangerUp added

--- source/DangerPopUp.py
+++ source/DangerPopUp.py
@@ -13,14 +13,14 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGTH = 1080
 
 class DangerPopUp(QWidget):
-    def __init__(self):
+    def __init__(self, init_parameters):
         super(DangerPopUp, self).__init__()
          ### 1.1. Instaurer la taille, la couleur de fond et le titre du de la fenêtre PopUp ###
         self.setGeometry(600, 300, SCREEN_WIDTH/3, SCREEN_HEIGTH/3)
         self.setWindowTitle("Averitissement")
         self.setStyleSheet("background-color: white;")
         self.initUI(self)
-    def initUI(self):  
+    def initUI(self, init_parameters):  
         ### 1.3. Mettre le logo du laboratoire dans le coin gauche de la fenêtre ###
         self.imageS2M = Image.open("img_S2M_JPG.jpg")
         self.petite_imageS2M = self.imageS2M.resize((200, 150))
