c301bb580d3f672329841b21efa43fc527e8247b
dibala21 <dielrosen98@gmail.com>
2022-04-01 11:56:08 -0400

    update des tests

--- source/testOdrive.py
+++ source/testOdrive.py
@@ -5,7 +5,8 @@ from odrive.enums import *
 import time
 import math
 
-def test_vitesse (vitesse, carte : odrive): 
+def test_vitesse (vitesse, carte : odrive):
+    carte.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION 
     carte.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL #pour démarrer le moteur
     carte.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL #set le mode vitesse
     start = time.time()
@@ -18,7 +19,8 @@ def test_vitesse (vitesse, carte : odrive):
     
     #Pour le test du changement de sens on peut utiliser la meme fonction avec une vitesse negative
 
-def test_couple (couple, carte : odrive): 
+def test_couple (couple, carte : odrive):
+    carte.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
     carte.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL #pour démarrer le moteur
     carte.axis0.controller.config.control_mode = CONTROL_MODE_TORQUE_CONTROL #set le mode torque
     carte.axis0.motor.config.torque_constant = 8.23 / 150
@@ -34,7 +36,7 @@ def test_couple (couple, carte : odrive):
         print("Test torque echec")
     else :
         print("Test torque reussi")
-    print("couple de fin", couple_fin)
+    print("couple de fin:", couple_fin, ", couple attendu:", couple)
 
 #Test 
 # Find a connected ODrive (this will block until you connect one)
@@ -47,5 +49,6 @@ while my_drive.axis0.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
 
 #decommenter pour effectuer les tests
-#  test_vitesse(1, my_drive)
-#  test_couple (1, my_drive)
+#test_vitesse(1, my_drive)
+test_couple (2, my_drive)
+my_drive.axis0.requested_state = AXIS_STATE_IDLE
