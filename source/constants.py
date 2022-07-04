4b1110ac3b2f5b1d4205f93c1c086ed92bbd8b44
Arsene BAERT <arsene.baert@etu.emse.fr>
2022-07-04 16:23:38 -0400

    Change from 0 and 1 to False and True

--- source/constants.py
+++ source/constants.py
@@ -3,12 +3,12 @@
 # Port paths
 ODRIVE_USB_PORT_PATH = ""
 REHASTIM_USB_PORT_PATH = ""
-USB_DRIVE_PORT_PATH = "COM4"
+USB_DRIVE_PORT_PATH = "COM5"
 
 # Screen resolution
 SCREEN_WIDTH = 1366
 SCREEN_HEIGHT = 768
 
 # Debug
-DEBUG_REHA = 1  # 0 : normal behaviour of software, 1 : only simulation function
-DEBUG_REHA_SHOW_COM = 0  # 0 : normal behaviour, 1 : print all communication with Rehastim
+DEBUG_REHA = True  # False : normal behaviour of software, True : only simulation function
+DEBUG_REHA_SHOW_COM = True  # False : normal behaviour, True : print all communication with Rehastim
